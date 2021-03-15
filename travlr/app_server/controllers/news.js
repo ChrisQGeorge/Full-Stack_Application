fdf42f79c3c5d8ff8763f883859393bf2a46370f
flywheel11 <37939187+flywheel11@users.noreply.github.com>
2021-03-15 12:38:34 -0400

    Replaced static content in news tab with json data

--- travlr/app_server/controllers/news.js
+++ travlr/app_server/controllers/news.js
@@ -1,6 +1,14 @@
+let fs1 = require('fs');
+let fs2 = require('fs');
+let fs3 = require('fs');
+let articles = JSON.parse(fs1.readFileSync('./data/newsArticles.json','utf8'));
+let latestNews = JSON.parse(fs2.readFileSync('./data/latestNews.json','utf8'));
+let tips = JSON.parse(fs3.readFileSync('./data/vacationTips.json','utf8'));
+
+
 /*GET travel page*/
 const news = (req, res)=>{
-    res.render('news', {title: 'Travlr Getaways'});
+    res.render('news', {title: 'Travlr Getaways', articles, tips, latestNews});
 };
 
 module.exports = {
